88ba9d73d1d30d48018ff6b057c5e06d7eedef33
Ayesh Udayanga <ayeshudayanga1743@gmail.com>
2024-04-17 19:50:16 +0530

    fix backend

--- backend/database.js
+++ backend/database.js
@@ -121,7 +121,7 @@ const readStudentInfo = async (id) => {
 }
 
 const addStudent = async (id, name, age, hometown) => {
-    const sql = `INSERT INTO student(id,name,age,hometown) values (?, ?, ?)`
+    const sql = `INSERT INTO student(id,name,age,hometown) values (?, ?, ?,?)`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql,[id,name,age,hometown])
@@ -138,7 +138,7 @@ const updateStudent = async (name, age, hometown, id) => {
     const sql = `UPDATE student SET name=?, age=?, hometown=? WHERE id=?`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql,[id,name,age,hometown])
+            .raw(sql,[name,age,hometown,id])
             .then(() => {
                 resolve({status: "Successfully updated Student"})
             })
